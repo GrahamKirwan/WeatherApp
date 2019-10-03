4df3f4c0a074121c70cec90d696f12ac680e9619
ISatByTheO <anthonyffernandes@gmail.com>
2019-10-03 18:31:56 -0400

    About page commit

---
+++ WeatherApp/WeatherApp/Models/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Models
+{
+    public class IndexViewModel
+    {
+        public string NameOfHome { get; set; }
+
+        public IndexViewModel(string nameOfHome)
+        {
+            NameOfHome = nameOfHome;
+        }
+    }
+}
